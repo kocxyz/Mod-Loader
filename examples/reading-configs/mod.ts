55c93d2a6842c8674ac7878350d0536056372116
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-10-05 21:54:24 +0200

    chore: Remove outdated example

--- examples/reading-configs/mod.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../types/api.d.ts" />
-import config from 'config';
-import logging from 'logging';
-
-const content = config.read('Hello World');
-logging.info(content as unknown as string);
