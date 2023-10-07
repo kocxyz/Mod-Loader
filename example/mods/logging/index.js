7d5e5d67b53baa0015dcf3ea19469a84c93ad93b
Tandashi <18377875+Tandashi@users.noreply.github.com>
2023-10-08 00:19:43 +0200

    feat: `logging` mod example

---
+++ example/mods/logging/index.js
@@ -0,0 +1,7 @@
+/// <reference path="../../../types/sandbox-api.d.ts" />
+import logging from 'logging';
+
+logging.debug('A debug message');
+logging.info('A info message');
+logging.warn('A warning message');
+logging.error('A error message');
